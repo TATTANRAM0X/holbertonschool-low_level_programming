9b5d6a0906abc47fb829842b0d82030c6021db1b
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-06-14 15:58:18 -0700

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints wheter a number is positive, negative or zero
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+char letra
+for (letra = "a" ; letra <= "z" ; letra++)
+{
+putchar(letra);
+}
+putchar("\n");
+return (0);
+}
