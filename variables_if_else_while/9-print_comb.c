700356a78f9bfd0e5b6d06de9930599d72fd13bc
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-06-15 10:07:56 -0700

    Combinations between numbers and simbol

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+char number;
+for (number = '0' ; number <= '9' ; number++)
+{
+putchar(number);
+if (number < '9')
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
