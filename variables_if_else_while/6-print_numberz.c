6bab2d575825172e4b1119a4f38e9544b1139eaf
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-06-15 08:12:28 -0700

    print number base 10

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+int number;
+for (number = '0' ; number <= '9' ; number++)
+{
+putchar(number);
+}
+putchar('\n');
+return (0);
+}
