21ede16286e04228aa39e2f5c6d130e4c1174d2b
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-06-17 12:19:58 -0700

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,13 +4,10 @@
  * main - Prints alphabet in lowercase
  * Return: 0 Success
  */
-
 void print_alphabet(void)
 {
 char letter;
 for (letter = 'a' ; letter <= 'z' ; letter++)
-{
 _putchar(letter);
-}
 _putchar('\n');
 }
