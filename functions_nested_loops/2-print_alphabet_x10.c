b2f1e760ca2f3679447fd0bfca43e11fa0e7e1ec
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-06-17 13:14:22 -0700

    alphabet x 10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - print alphabet
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+char letter;
+int repeat
+for (repeat = 0; repeat <= 10; repeat++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar (letter);
+}
+_putchar ('\n');
+}
+}
