266c8bdb8d385730048f7ab91ef46a940891e47d
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-06-17 14:58:31 -0700

    check lowercase

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,16 +2,16 @@
 
 /**
 * lowercase check
-* 
+* @c: check 
 * Return: 0
 */
 
 int _islower(int c)
 {
-char lowercase;
-for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+char check_lowercase;
+for (check_lowercase = 'a'; check_lowercase <= 'z'; check_lowercase++)
 {
-if (c == lowercase)
+if (c == check_lowercase)
 return (1);
 }
 return (0);
