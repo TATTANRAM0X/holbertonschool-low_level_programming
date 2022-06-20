1d90cb5c70dfbb96ec23b706fe3889dd3b602e97
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-06-20 15:48:10 -0700

    repeat numbers x 10

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* more_numbers - print numbers x10
+* Return: 0
+*/
+void more_numbers(void)
+{
+int number;
+int repeat;
+int base = 10;
+int min_repeat = 1;
+int max_repeat = 10;
+int min_number = 0;
+int max_number = 14;
+int repeat_cycle = 9;
+for (repeat = min_repeat; repeat <= max_repeat; repeat++)
+{
+for (number = min_number; number <= max_number; number++)
+{
+if (number > repeat_cycle)
+{
+putchar ((number / base) + '0');
+}
+putchar ((number % base) + '0');
+}
+putchar ('\n');
+}
+}
