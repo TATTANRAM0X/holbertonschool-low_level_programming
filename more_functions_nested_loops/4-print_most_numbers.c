ec55ff4f847d9a34f84ab627adf28f9f0978a4a3
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-06-20 15:10:10 -0700

    less 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_numbers - print numbers 0 to 9
+* print_most_numbers - print numbers not 2 and 4
 * Return: 0
 */
 void print_most_numbers(void)
@@ -9,7 +9,7 @@ int number;
 for (number = '0'; number <= '9'; number++)
 {
 if (!(number == '2' || number == '4'))
-putchar(number);
+putchar (number);
 }
-putchar('\n');
+putchar ('\n');
 }
