c6b2aace1cd09bcbebe9709c894de22ddda6faab
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-06-20 17:11:49 -0700

    Print diagonal line

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,15 +6,15 @@
 */
 void print_diagonal(int n)
 {
-int variable_diagonal_line;
-int variable_new_space;
+int var_diagonal_line;
+int var_new_space;
 int space = ' ';
 int diagonal_line = 92;
 if (n > 0)
 {
-for (variable_diagonal_line = 0; variable_diagonal_line < n; variable_diagonal_line++)
+for (var_diagonal_line = 0; var_diagonal_line < n; var_diagonal_line++)
 {
-for (variable_new_space = 0; variable_new_space < variable_diagonal_line; variable_new_space++)
+for (var_new_space = 0; var_new_space < var_diagonal_line; var_new_space++)
 {
 putchar(space);
 }
