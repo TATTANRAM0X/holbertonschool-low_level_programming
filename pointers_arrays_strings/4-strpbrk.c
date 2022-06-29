76dc4ef43b22e125ae6e6d2c85f1f18ab4b4e416
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-06-29 10:26:35 -0700

    Function that searches a string for any of a set of bytes

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -14,15 +14,15 @@ char *_strpbrk(char *s, char *accept)
 
 	while (s[x] != '\0')
 	{
-		y=0;
+		y = 0;
 		while (accept[y] != '\0')
 		{
 			if (s[x] == accept[y])
-				{	
-					return (s + x);
-				}	
-				y++;
-		}	
+			{
+				return (s + x);
+			}
+			y++;
+		}
 		x++;
 	}
 	return (0);
