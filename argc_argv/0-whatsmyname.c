099d755ddbc1c3144cfc2e1976f10b8eac3cdac7
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-07-06 10:55:50 -0700

    All

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main - main declaration for funtion
+  * @argc: indicate size of argv
+  * @argv: program arguments 
+  * Return: 0
+  */
+
+/** prints the name of a program, if the name changes, the new name is printed.
+ * The program prints the string located at position 0
+ * which is equivalent to the name of the program used for execution
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	printf("%s\n", argv[0]);
+
+	return (EXIT_SUCCESS);
+}
