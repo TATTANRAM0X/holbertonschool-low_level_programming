5873cc74be6f6b9d37aeea2f80dca0978762fd56
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-07-06 11:01:27 -0700

    prints the name of the program that executes it

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,13 +3,11 @@
 /**
   * main - main declaration for funtion
   * @argc: indicate size of argv
-  * @argv: program arguments 
+  * @argv: program arguments
   * Return: 0
-  */
-
-/** prints the name of a program, if the name changes, the new name is printed.
- * The program prints the string located at position 0
- * which is equivalent to the name of the program used for execution
+  * prints the name of a program, if the name changes, the new name is printed.
+  * The program prints the string located at position 0
+  * which is equivalent to the name of the program used for execution
  */
 
 int main(int argc, char *argv[])
