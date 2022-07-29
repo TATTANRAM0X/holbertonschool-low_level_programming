345a1a3397dfaec186c3730cf1c1b91c83c6756f
Jhonatan Ramos <jhonatan.ramox@gmail.com>
2022-07-29 13:59:15 -0500

    function that prints all the elements of a listint_t list

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <stdio.h>
 
-/*
+/**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
@@ -20,7 +20,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/*Prototypes*/
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
